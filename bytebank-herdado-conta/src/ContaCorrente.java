6756bbce18cdeb9f18e93991c7ae0879f44c0b15
Leonardo Müller de Lima <86493061+Lemuller-21@users.noreply.github.com>
2021-07-23 20:31:36 -0300

    Add files via upload

---
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,19 @@
+
+public class ContaCorrente extends Conta {
+	
+	public ContaCorrente(int agencia, int numero) {
+		super(agencia, numero);
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		return super.saca(valor + 0.2);
+	}
+	
+	@Override
+	public void deposita(double valor) {
+		if (valor > 0) {
+			super.saldo += valor;
+		}
+	}
+}
